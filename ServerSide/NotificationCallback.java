6d5122297fbcaaf51f5c1d7f9b9126c0c142f710
Hazem Zidan <137702216+hazemzidan4545@users.noreply.github.com>
2025-05-29 19:11:23 +0300

    Delete ServerSide directory

--- ServerSide/NotificationCallback.java
+++
@@ -1,9 +0,0 @@
-package ServerSide;
-
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface NotificationCallback extends Remote {
-    // Callback method for notifiers to receive confirmation
-    public void notificationPublished(Event event, boolean success) throws RemoteException;
-}
--- ServerSide/SubscriberCallback.java
+++
@@ -1,9 +0,0 @@
-package ServerSide;
-
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface SubscriberCallback extends Remote {
-    // Callback method for subscribers to receive notifications
-    public void receiveNotification(Event event) throws RemoteException;
-}
